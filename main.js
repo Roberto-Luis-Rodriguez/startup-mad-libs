6cc3caf5bb9e04fc6bce251fc89dca9f22cbf716
Roberto-Luis-Rodriguez <robertorodriguezjr86@gmail.com>
2017-11-13 15:13:28 -0500

    My submitted code.

---
+++ main.js
@@ -0,0 +1,11 @@
+let companyName = 'New Digital Enterprises, Inc.';
+let anOffering = 'a revolutionary new platform';
+let aTargetAudience = 'the world';
+let solveAProblem = 'create new dynamic markets for business';
+let secretSauce = 'by building invite-only Internets';
+
+// your code, here
+let mypitch = document.getElementById('main');
+mypitch.innerHTML = "My company is " + companyName + ".\nIt is " + anOffering + ".\nMy target audience is " + aTargetAudience + ".\n My company will " + solveAProblem + "\n" + secretSauce + " ." ;
+
+console.log(mypitch);
